ba46e800ce60edf03f5e75ebe82e797a45df964a
Uriah M. Clemmer IV <uriah@nihil.co>
2018-07-18 13:24:31 -0400

    Fixed home link.

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -1,6 +1,6 @@
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+        <li class="breadcrumb-item"><a href="{{ url()->route('home') }}"><i class="fas fa-home"></i></a></li>
         @foreach ($breadcrumbs as $breadcrumb)
 
             @if ($breadcrumb['url'])
