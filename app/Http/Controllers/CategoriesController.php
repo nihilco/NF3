a0db5b7f00878c23d20b14ff776dcdb47530732d
Uriah M. Clemmer IV <uriah@nihil.co>
2018-08-25 12:16:47 -0400

    Inital Categories build.

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoriesController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+	$categories = Category::all();
+	return view('categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+	return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+	$this->validate(request(), [
+	    'name' => 'required|string',
+	    'description' => 'required|string',
+	]);
+
+	$category = new Category();
+
+	$category->creator_id = auth()->id();
+	$category->owner_id = auth()->id();
+	$category->name = request('name');
+	$category->description = request('description');
+
+	$category->save();
+
+	\Session::flash('message', 'Category successfully created.');
+	\Session::flash('alert-class', 'alert-success');
+
+	if(request()->expectsJson()) {
+	    return response()->json($category, 201);
+	}
+
+	return redirect('/categories');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+	if(request()->expectsJson()) {
+	    return $category;
+	}
+
+	return view('categories.show', compact(['category']));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+	return view('categories.edit', compact(['category']));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+	$this->validate(request(), [
+	    'name' => 'required|string',
+	    'description' => 'required|string',
+	]);
+
+	$category->name = request('name');
+	$category->description = request('description');
+
+	$category->save();
+
+	\Session::flash('message', 'Category successfully updated.');
+	\Session::flash('alert-class', 'alert-success');
+
+	if(request()->expectsJson()) {
+	    return response()->json($category, 200);
+	}
+
+	return redirect($category->path());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+	$category->delete();
+
+	\Session::flash('message', 'Category successfully deleted.');
+	\Session::flash('alert-class', 'alert-success');
+
+	if(request()->expectsJson()) {
+	    return response()->json(null, 204);
+	}
+
+	return back();
+    }
+
+    //
+    public function list()
+    {
+        $categories = Category::all();
+
+	if(request()->expectsJson()) {
+	    return $categories;
+	}
+
+	return view('categories.list', compact(['categories']));
+    }
+}
---
+++ database/migrations/2018_07_01_005002_create_categories_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->string('name');
+	    $table->text('description');
+	    $table->softDeletes();
+            $table->timestamps();
+
+	    $table->unsignedInteger('creator_id');
+	    $table->unsignedInteger('owner_id');
+
+	    $table->foreign('creator_id')
+		->references('id')
+		->on('users')
+		->onDelete('cascade');
+
+	    $table->foreign('owner_id')
+		->references('id')
+		->on('users')
+		->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
