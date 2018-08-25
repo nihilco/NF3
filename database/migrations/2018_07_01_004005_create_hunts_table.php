1c269b2355e5b6a514bced25b7f4f1eb5b5bbf88
Uriah M. Clemmer IV <uriah@nihil.co>
2018-08-25 12:55:04 -0400

    Inital Hunts build.

---
+++ database/migrations/2018_07_01_004005_create_hunts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateHuntsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hunts', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->enum('type', ['beginner', 'intermediate', 'advanced']);
+	    $table->unsignedInteger('award');
+	    $table->unsignedInteger('skill');
+	    $table->unsignedInteger('animals_killed_count')->default(0);
+	    $table->softDeletes();
+            $table->timestamps();
+
+	    $table->unsignedInteger('creator_id');
+	    $table->unsignedInteger('owner_id');
+
+	    $table->foreign('creator_id')
+		->references('id')
+		->on('users')
+		->onDelete('cascade');
+
+	    $table->foreign('owner_id')
+		->references('id')
+		->on('users')
+		->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hunts');
+    }
+}
