2dec2c476ce6e5cfb20b4074d61a9e90faf0b01e
Uriah M. Clemmer IV <uriah@nihil.co>
2018-12-05 14:35:28 -0500

    CSS cleanup.

--- resources/views/layouts/construction.blade.php
+++ resources/views/layouts/construction.blade.php
@@ -9,12 +9,8 @@
 
     <title>Under Construction | {{ config('app.name') }}</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-    <!-- Fontawesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-
+    @include('partials.analytics')
+    
     <!-- App CSS -->
     <link rel="stylesheet" href="{{ url('/css/app.css?t=' . time()) }}" />
     
