9128ffb2b149cb275a9012258268f62306a71ddb
Uriah M. Clemmer IV <uriah@nihil.co>
2018-12-05 14:40:19 -0500

    That last one was a mess.  CSS cleanup.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,27 +6,12 @@
     <link rel="icon" href="/favicon.ico">
 
     <title>@yield('title') | {{ config('app.name') }}</title>
-<<<<<<< HEAD
 
     @include('partials.analytics')    
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-    <!-- Fontawesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-=======
->>>>>>> development
-
-    @include('partials.analytics')
-      
     <!-- App CSS -->
     <link rel="stylesheet" href="{{ url('/css/app.css?t=' . time()) }}" />
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> development
   </head>
 
   <body>
